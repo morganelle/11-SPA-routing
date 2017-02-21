2926641662834eda1b57f4349d46cfd72875154e
Morgan Nomura <morganelle@gmail.com>
2017-02-21 09:51:46 -0800

    completed first todo items

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -5,7 +5,11 @@
   // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-
+  articleController.init = function() {
+    console.log('article controller running');
+    // $('#articles').text('Welcome to articles page');
+    // $('#about').empty();
+  }
 
   module.articleController = articleController;
 })(window);
